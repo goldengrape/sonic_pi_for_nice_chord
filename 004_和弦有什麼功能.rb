1a3c594bfc953fb6f0f010018bdb6ba4c6570db1
GoldenGrape <goldengrape@goldengrape.org>
2019-11-18 16:17:22 +0800

    004, 用状态转移矩阵来实现和弦的转换

---
+++ 004_和弦有什麼功能.rb
@@ -0,0 +1,57 @@
+##| 好和弦的第 4 集
+##| 和弦有什麼功能
+
+##| define scale
+s=scale(:C4, :major, num_octaves: 2)
+
+##| define kinds of chords
+tonic_chord=(ring s[0],s[2],s[4])
+sub_dominant_chord=(ring s[3],s[5],s[7])
+dominant_chord=(ring s[4],s[6],s[8])
+dominant_unstable_chord=(ring s[4],s[6],s[8],s[10])
+
+
+chord_list=[tonic_chord,\
+            sub_dominant_chord,\
+            dominant_chord,\
+            dominant_unstable_chord]
+
+##| ##| demo chords
+##| chord_list.each do |c|
+##|   play_chord c
+##|   sleep 1
+##| end
+
+
+##| transition_matrix:
+##| from home->home [0,0] chance=0.1,
+##| from home->bridge [0,1] chance=0.5
+##| from outside->home [2,0] chance=0.5
+##| from outside->bridge [2,1] chance=0
+
+transition_matrix=\
+  [[0.1, 0.5, 0.3, 0.1],
+   [0.2, 0, 0.5,0.3],
+   [0.5, 0, 0.3,0.2],
+   [0.5, 0, 0.3,0.2]]
+  
+# random choose by chance
+def picker(options, chances)
+  current, max = 0, chances.inject(:+)
+  random_value = rrand(0,max)
+  (0..options.length).each do |i|
+    current += chances[i]
+    return options[i] if random_value <=current
+  end
+end
+
+current_id=0
+live_loop :chord_trans do
+  ##| choose next chord based on transition_matrix
+  next_id=picker([0,1,2,3],transition_matrix[current_id])
+  play_chord chord_list[next_id]
+  play_pattern_timed chord_list[next_id].take(3),\
+    [0.25,0.25,0.5]
+  sleep 1
+  current_id=next_id
+end
